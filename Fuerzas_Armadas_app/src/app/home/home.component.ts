0f6036a2143e2ce0a1e277d489acb02a766a11bb
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-24 00:15:33 +0200

    imports en home.component
    Añado imports necesarios para usar titulaciones component y una sección en home.component.html para titulaciones.

--- Fuerzas_Armadas_app/src/app/home/home.component.ts
+++ Fuerzas_Armadas_app/src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { TitulacionesComponent} from '../titulaciones//titulaciones.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TitulacionesComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
