6db50af0a80d0540cc1e9ee7c8ce506e1521eda3
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-24 00:13:38 +0200

    Componente Titulaciones
    Añado:
    titulaciones.component.css
    titulaciones.component.html
    titulaciones.component.ts

---
+++ Fuerzas_Armadas_app/src/app/titulaciones/titulaciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titulaciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './titulaciones.component.html',
+  styleUrl: './titulaciones.component.css'
+})
+export class TitulacionesComponent {
+
+}
