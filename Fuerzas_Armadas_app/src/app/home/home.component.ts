85ef5daa176fba9f1940f1734c07707ecdbbbd21
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-24 19:21:41 +0200

    cambio titulaciones por cuerpos en home.component
    cambio titulaciones por cuerpos en home.component

--- Fuerzas_Armadas_app/src/app/home/home.component.ts
+++ Fuerzas_Armadas_app/src/app/home/home.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { TitulacionesComponent} from '../titulaciones//titulaciones.component';
+import { CuerposComponent } from '../cuerpos/cuerpos.component';
 import { CommonModule } from '@angular/common';
+import { CuerposInterface } from '../cuerposInterface';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    TitulacionesComponent
+    CuerposComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  cuerpos: CuerposInterface = {
+
+    cuerpo: 'Cuerpos generales e infantería de Marina',
+    titulacion: 'Titulaciones de grado del ANEXO I de la Orden Def. 462/2022 de 25 de mayo',
+    requisitos_edad: 'Ingreso directo con exigencia previa de titulación de grado universitario: 27 años',
+    pais: 'ESP',
+    photo: `https://s3.abcstatics.com/Media/201202/28/marinabosnia--644x362.jpg"`,
+  };
 }
