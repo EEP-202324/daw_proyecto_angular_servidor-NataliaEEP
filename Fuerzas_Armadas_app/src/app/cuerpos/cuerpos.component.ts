9d81e99539922be87e6a9d38a7d329a7b663cc8c
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-24 19:21:05 +0200

    Componente Cuerpos
    Cambio el nombre del componente titulaciones por Cuerpos que es mas adecuado

---
+++ Fuerzas_Armadas_app/src/app/cuerpos/cuerpos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titulaciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './cuerpos.component.html',
+  styleUrl: './cuerpos.component.css'
+})
+export class CuerposComponent {
+
+}
