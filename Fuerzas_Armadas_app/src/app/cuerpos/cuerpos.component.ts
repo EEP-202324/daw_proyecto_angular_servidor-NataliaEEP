0a538c64e8a975949f85387ed3e5dbaceaccd350
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-24 23:48:13 +0200

    cuerpos.component.ts - @Input() cuerpos
    Añado imports necesarios y un @input para que la propiedad cuerpos de la clase CuerposComponent pueda recibir datos del componente padre. Esta propiedad es utilizada para pasar datos de un componente padre a un componente hijo.
    cuerpos: Es el nombre de la propiedad que se define en el componente CuerposComponent. Este es el nombre que se utilizará para pasar datos al componente desde su componente padre.
    CuerposInterface: Esto es el tipo de datos que se espera para la propiedad cuerpos. CuerposInterface es una interfaz que define la estructura de los datos que se esperan.

--- Fuerzas_Armadas_app/src/app/cuerpos/cuerpos.component.ts
+++ Fuerzas_Armadas_app/src/app/cuerpos/cuerpos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CuerposInterface } from '../cuerposInterface';
 
 @Component({
   selector: 'app-titulaciones',
@@ -8,5 +10,6 @@ import { Component } from '@angular/core';
   styleUrl: './cuerpos.component.css'
 })
 export class CuerposComponent {
+  @Input() cuerpos!: CuerposInterface;
 
 }
