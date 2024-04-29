c570329909d053fbc10edc02eb8ef1f310374312
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-29 20:12:04 +0200

    Proyecto base de Spring Initializr - Cuerpos
    Proyecto base de Spring Initializr - Cuerpos

---
+++ cuerpo/src/main/java/com/example/cuerpo/CuerpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuerpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuerpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuerpoApplication.class, args);
+	}
+
+}
---
+++ cuerpo/src/test/java/com/example/cuerpo/CuerpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuerpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuerpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
