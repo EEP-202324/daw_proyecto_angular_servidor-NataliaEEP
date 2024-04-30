2fce74d3afae49e82b3bdc1d9b1cd90d1b5702c6
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-04-30 22:34:02 +0200

    Modificar cuerpo - Implementación
    Implementación de código para modificar cuerpo, con botón dentro de details.component para entrar en el formulario y modificar lo que se desee y boton de cancelar la modificación. Leemos el id correspondiente del cuerpo y modificamos a ese id lo necesario.

--- Fuerzas_Armadas_app/src/app/details/details.component.ts
+++ Fuerzas_Armadas_app/src/app/details/details.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { CuerposService } from '../cuerpos.service';
 import { CuerposInterface } from '../cuerposInterface';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Peticion } from '../peticion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -16,49 +15,80 @@ import { Peticion } from '../peticion';
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-
-export class DetailsComponent {
-
-  route: ActivatedRoute = inject(ActivatedRoute);
-  cuerposService = inject(CuerposService);
+export class DetailsComponent implements OnInit {
   cuerposLocation: CuerposInterface | undefined;
+  applyForm: FormGroup;
+  mostrarDatos = false; // Variable para controlar la visibilidad de los datos existentes
 
-  applyForm = new FormGroup({
-    cuerpo: new FormControl(''),
-    titulacion: new FormControl(''),
-    requisitos_edad: new FormControl(''),
-    pais: new FormControl(''),
-    photo: new FormControl(''),
-    pdf: new FormControl('')
-  });
-
-  constructor() {
-
-    const cuerpoId = Number(this.route.snapshot.params['id']);
-    this.cuerposService.getCuerpo(cuerpoId).subscribe(
-      cuerpo => {
-      this.cuerposLocation = cuerpo;
+  constructor(
+    private route: ActivatedRoute,
+    private cuerposService: CuerposService
+  ) {
+    this.applyForm = new FormGroup({
+      cuerpo: new FormControl(''),
+      titulacion: new FormControl(''),
+      requisitos_edad: new FormControl(''),
+      pais: new FormControl(''),
+      photo: new FormControl(''),
+      pdf: new FormControl('')
     });
   }
 
-  submitApplication() {
-    this.cuerposService.submitApplication(
-      this.applyForm.value.cuerpo ?? '',
-      this.applyForm.value.titulacion ?? '',
-      this.applyForm.value.requisitos_edad ?? '',
-      this.applyForm.value.pais ?? '',
-      this.applyForm.value.photo ?? '',
-      this.applyForm.value.pdf ?? ''
-    );
-    const peticion: Peticion = {...(this.applyForm.value)};
-    this.cuerposService.enviarPeticion(peticion).subscribe({
-      next: peticion => {
-        alert(peticion.id);
-        console.log(peticion)
+  ngOnInit(): void {
+    // Obtener el ID del cuerpo de la URL
+    const cuerpoId = Number(this.route.snapshot.params['id']);
+    // Obtener los datos del cuerpo correspondiente al ID
+    this.cuerposService.getCuerpo(cuerpoId).subscribe({
+      next: cuerpo => {
+        this.cuerposLocation = cuerpo;
+        // Rellenar el formulario con los datos del cuerpo obtenido
+        this.applyForm.patchValue({
+          cuerpo: cuerpo.cuerpo,
+          titulacion: cuerpo.titulacion,
+          requisitos_edad: cuerpo.requisitos_edad,
+          pais: cuerpo.pais,
+          photo: cuerpo.photo,
+          pdf: cuerpo.pdf
+        });
       },
       error: error => {
-        console.log(error)
+        console.log('Error al obtener el cuerpo', error);
       }
-  });
+    });
+  }
+
+  onSubmit(): void {
+    if (this.applyForm.valid) {
+      // Obtener los datos del formulario
+      const cuerpoDatos = this.applyForm.value;
+      // Conservar el ID original en el objeto de datos
+      cuerpoDatos.id = this.cuerposLocation?.id;
+
+      // Modificar el cuerpo con los nuevos datos
+      this.cuerposService.modificarCuerpo(cuerpoDatos).subscribe({
+        next: cuerpoModificado => {
+          console.log('Cuerpo modificado', cuerpoModificado);
+          alert('Cuerpo modificado exitosamente');
+        },
+        error: error => {
+          console.log('Error al modificar el cuerpo', error);
+          alert('Ha ocurrido un error al modificar el cuerpo');
+        }
+      });
+    }
+  }
+
+  // Método para cambiar la visibilidad de los datos existentes en el formulario
+  toggleDatos(): void {
+    this.mostrarDatos = !this.mostrarDatos;
+  }
+
+  toggleFormulario(): void {
+    this.mostrarDatos = true;
+  }
+
+  cancelar(): void {
+    // Cerrar el formulario de modificación
+    this.mostrarDatos = false;
   }
 }
