21bed4133fdf3682dfd69fa83f7aca6e62832efb
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-06 19:56:23 +0200

    Creo Class CuerpoController y  método findById
     Esta clase es un controlador REST y que cada método de esta clase devolverá directamente el objeto serializado en la respuesta HTTP.
     Este método maneja las solicitudes GET para obtener información sobre un cuerpo militar específico.

---
+++ cuerpo/src/main/java/com/example/cuerpo/CuerpoController.java
@@ -0,0 +1,20 @@
+package com.example.cuerpo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cuerpos")
+class CuerpoController {
+   
+   @GetMapping("/{requestedId}")
+   private ResponseEntity<Cuerpo> findById() {
+      Cuerpo cuerpo = new Cuerpo(99, "Navy", "Titulacion Universitaria", "37 años maximo", "USA",
+    		  "https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG", 
+    		  "https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf");
+      return ResponseEntity.ok(cuerpo);
+   }
+}
