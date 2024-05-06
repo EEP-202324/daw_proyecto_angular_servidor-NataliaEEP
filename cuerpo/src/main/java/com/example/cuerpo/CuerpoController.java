a2d32f5331ab90a65c5acd0451e752070f483709
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-06 22:10:41 +0200

    Update CuerpoController.java
    Inyecto CuerpoRepository en CuerpoController y actualizo CuerpoController para utilizar el método findById  en CuerpoRepository

--- cuerpo/src/main/java/com/example/cuerpo/CuerpoController.java
+++ cuerpo/src/main/java/com/example/cuerpo/CuerpoController.java
@@ -1,5 +1,7 @@
 package com.example.cuerpo;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,16 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cuerpos")
 class CuerpoController {
-   
-   @GetMapping("/{requestedId}")
-   private ResponseEntity<Cuerpo> findById(@PathVariable int requestedId) {
-       if (requestedId == 99) {
-    	   Cuerpo cuerpo = new Cuerpo(99L, "Navy", "Titulacion Universitaria", "37 años maximo", "USA",
-    	    		  "https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG", 
-    	    		  "https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf");
-           return ResponseEntity.ok(cuerpo);
-       } else {
-           return ResponseEntity.notFound().build();
-       }
-   }
+	private final CuerpoRepository cuerpoRepository;
+
+	private CuerpoController(CuerpoRepository cuerpoRepository) {
+	      this.cuerpoRepository = cuerpoRepository;
+	   }
+
+	@GetMapping("/{requestedId}")
+	private ResponseEntity<Cuerpo> findById(@PathVariable Long requestedId) {
+	    Optional<Cuerpo> cuerpoOptional = cuerpoRepository.findById(requestedId);
+	    if (cuerpoOptional.isPresent()) {
+	        return ResponseEntity.ok(cuerpoOptional.get());
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
 }
