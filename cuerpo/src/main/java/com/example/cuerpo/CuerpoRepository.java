0dd948e8dafdf8987139a4fb65c9420d293f1903
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-07 22:07:59 +0200

    PagingAndSortingRepository<Cuerpo, Long>
    Esta interfaz extiende CrudRepository y agrega soporte para la paginación y la clasificación de los resultados de la consulta.

--- cuerpo/src/main/java/com/example/cuerpo/CuerpoRepository.java
+++ cuerpo/src/main/java/com/example/cuerpo/CuerpoRepository.java
@@ -1,7 +1,8 @@
 package com.example.cuerpo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CuerpoRepository extends CrudRepository<Cuerpo, Long>{
+public interface CuerpoRepository extends CrudRepository<Cuerpo, Long>, PagingAndSortingRepository<Cuerpo, Long>{
 
 }
