3a4243a1ee4675b516721d4caea15e3db2393a40
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-07 21:21:26 +0200

    Rectifico el data.sql
    Rectifico el data.sql

--- cuerpo/src/test/resources/data.sql
+++ cuerpo/src/test/resources/data.sql
@@ -7,11 +7,5 @@ VALUES (99, 'Navy', 'Titulacion Universitaria', '37 años maximo', 'USA',
     	'https://www.army.mil/pdf/Army_Regulations.pdf'),
     	(101, 'Air Force', 'Licenciatura en Aeronáutica', '35 años máximo', 'USA', 
     	'https://www.airforce.com.br/images/noticias/origem-da-forca-aerea-brasileira.jpg', 
-    	'https://www.airforce.com.br/pdf/airforce_regulations.pdf'),
-    	(100, 'Army', 'Otra Titulacion', '40 años maximo', 'UK', 
-    	'https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG', 
-    	'https://www.army.mil/pdf/Army_Regulations.pdf'),
-    	(101, 'Air Force', 'Licenciatura en Aeronáutica', '35 años máximo', 'USA', 
-    	'https://www.airforce.com.br/images/noticias/origem-da-forca-aerea-brasileira.jpg', 
     	'https://www.airforce.com.br/pdf/airforce_regulations.pdf');
     	
