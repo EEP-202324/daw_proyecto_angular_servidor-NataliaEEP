8873fbe8cbc73bfa1ed82d1a2a8d2dc2f6005d72
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-07 11:12:43 +0200

    Añado más datos a data..sql
    Añado más datos a data.sql

--- cuerpo/src/test/resources/data.sql
+++ cuerpo/src/test/resources/data.sql
@@ -1,4 +1,17 @@
 INSERT INTO cuerpo (id, cuerpo, titulacion, requisitos_edad, pais, photo, pdf) 
 VALUES (99, 'Navy', 'Titulacion Universitaria', '37 años maximo', 'USA', 
         'https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG', 
-        'https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf');
+        'https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf'),
+        (100, 'Army', 'Otra Titulacion', '40 años maximo', 'UK', 
+    	'https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG', 
+    	'https://www.army.mil/pdf/Army_Regulations.pdf'),
+    	(101, 'Air Force', 'Licenciatura en Aeronáutica', '35 años máximo', 'USA', 
+    	'https://www.airforce.com.br/images/noticias/origem-da-forca-aerea-brasileira.jpg', 
+    	'https://www.airforce.com.br/pdf/airforce_regulations.pdf'),
+    	(100, 'Army', 'Otra Titulacion', '40 años maximo', 'UK', 
+    	'https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG', 
+    	'https://www.army.mil/pdf/Army_Regulations.pdf'),
+    	(101, 'Air Force', 'Licenciatura en Aeronáutica', '35 años máximo', 'USA', 
+    	'https://www.airforce.com.br/images/noticias/origem-da-forca-aerea-brasileira.jpg', 
+    	'https://www.airforce.com.br/pdf/airforce_regulations.pdf');
+    	
