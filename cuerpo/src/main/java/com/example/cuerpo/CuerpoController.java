abd3f060a1039b6bdaba4abc52a7f34a2dfc3630
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-07 11:02:15 +0200

    @PostMapping en CuerpoController
    Implementación de @PostMapping en CuerpoController

--- cuerpo/src/main/java/com/example/cuerpo/CuerpoController.java
+++ cuerpo/src/main/java/com/example/cuerpo/CuerpoController.java
@@ -1,12 +1,16 @@
 package com.example.cuerpo;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cuerpos")
@@ -25,5 +29,15 @@ class CuerpoController {
 	    } else {
 	        return ResponseEntity.notFound().build();
 	    }
+	}
+	
+	@PostMapping
+	private ResponseEntity<Void> createCuerpo(@RequestBody Cuerpo newCuerpoRequest, UriComponentsBuilder ucb) {
+	   Cuerpo savedCuerpo = cuerpoRepository.save(newCuerpoRequest);
+	   URI locationOfNewCuerpo = ucb
+	            .path("cuerpos/{id}")
+	            .buildAndExpand(savedCuerpo.getId())
+	            .toUri();
+	   return ResponseEntity.created(locationOfNewCuerpo).build();
 	}
 }
