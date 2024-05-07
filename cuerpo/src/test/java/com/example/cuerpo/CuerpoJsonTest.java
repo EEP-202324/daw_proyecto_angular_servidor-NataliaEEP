60f106d95f1ad90b436da9cfc2fe71e22dcf3854
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-07 21:32:59 +0200

    cashCardListSerializationTest y cashCardListDeserializationTest
    cashCardListSerializationTest: test que verifica si una lista de objetos Cuerpo se serializa correctamente a formato JSON.
    cashCardListDeserializationTest: Test que verifica si un JSON dado se deserializa correctamente en una lista de objetos Cuerpo.

--- cuerpo/src/test/java/com/example/cuerpo/CuerpoJsonTest.java
+++ cuerpo/src/test/java/com/example/cuerpo/CuerpoJsonTest.java
@@ -43,7 +43,7 @@ class CuerpoJsonTest {
         Cuerpo cuerpo = new Cuerpo(99L, "Navy", "Titulacion Universitaria", "37 años maximo", "USA", 
         		"https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG",
         		"https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf");
-        assertThat(json.write(cuerpo)).isStrictlyEqualToJson("/example/cuerpo/expected.json"); 
+        assertThat(json.write(cuerpo)).isStrictlyEqualToJson("/example/cuerpo/single.json"); 
         assertThat(json.write(cuerpo)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cuerpo)).extractingJsonPathNumberValue("@.id")
                 .isEqualTo(99);
@@ -73,5 +73,23 @@ class CuerpoJsonTest {
        assertThat(json.parseObject(expected).getPais()).isEqualTo("USA");
        assertThat(json.parseObject(expected).getPhoto()).isEqualTo("https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG");
        assertThat(json.parseObject(expected).getPdf()).isEqualTo("https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf");
+    }
+    
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+       assertThat(jsonList.write(cuerpos)).isStrictlyEqualToJson("/example/cuerpo/list.json");
+    }
+    
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+       String expected="""
+       		[
+       		    { "id": 99, "cuerpo": "Navy", "titulacion": "Titulacion Universitaria", "requisitos_edad": "37 años maximo", "pais": "USA", "photo": "https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG", "pdf": "https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf" },
+       		    { "id": 100, "cuerpo": "Army", "titulacion": "Otra Titulacion", "requisitos_edad": "40 años maximo", "pais": "UK", "photo": "https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG", "pdf": "https://www.army.mil/pdf/Army_Regulations.pdf" },
+       			{ "id": 101, "cuerpo": "Air Force", "titulacion": "Licenciatura en Aeronáutica", "requisitos_edad": "35 años máximo", "pais": "USA", "photo": "https://www.airforce.com.br/images/noticias/origem-da-forca-aerea-brasileira.jpg", "pdf": "https://www.airforce.com.br/pdf/airforce_regulations.pdf" }
+       		]
+             """;
+       assertThat(jsonList.parse(expected)).usingRecursiveComparison().isEqualTo(cuerpos);
+//       assertThat(jsonList.parse(expected)).isEqualTo(cuerpos);
     }
 }
