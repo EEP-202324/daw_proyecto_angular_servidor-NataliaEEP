29788ed6ff7c138af28abbcea475bd047d95841e
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-07 21:24:47 +0200

    shouldReturnAllCuerposWhenListIsRequested
    Implemento test shouldReturnAllCuerposWhenListIsRequested,
    test verifica el comportamiento de un endpoint que devuelve una lista de cuerpos.

--- cuerpo/src/test/java/com/example/cuerpo/CuerpoApplicationTests.java
+++ cuerpo/src/test/java/com/example/cuerpo/CuerpoApplicationTests.java
@@ -16,8 +16,10 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
+import net.minidev.json.JSONArray;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+//@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CuerpoApplicationTests {
 
 	@Autowired
@@ -62,9 +64,9 @@ class CuerpoApplicationTests {
 	}
 
 	@Test
-	//@DirtiesContext	
+	@DirtiesContext	
 	void shouldCreateANewCuerpo() {
-		Cuerpo newCuerpo = new Cuerpo(44L, "Navy", "Titulacion Universitaria", "37 años maximo", "USA",
+		Cuerpo newCuerpo = new Cuerpo(null, "Navy", "Titulacion Universitaria", "37 años maximo", "USA",
 				"https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG",
 				"https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf");
 		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cuerpos", newCuerpo, Void.class);
@@ -76,7 +78,7 @@ class CuerpoApplicationTests {
 
 		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
 
-		Long id = documentContext.read("$.id");
+		Number id = documentContext.read("$.id");
 		String cuerpo = documentContext.read("$.cuerpo");
 		String titulacion = documentContext.read("$.titulacion");
 		String requisitos_edad = documentContext.read("$.requisitos_edad");
@@ -84,13 +86,48 @@ class CuerpoApplicationTests {
 		String photo = documentContext.read("$.photo");
 		String pdf = documentContext.read("$.pdf");
 
-		assertThat(id).isNotNull();
-		assertThat(cuerpo).isEqualTo("ValorEsperadoParaCuerpo");
-		assertThat(titulacion).isEqualTo("ValorEsperadoParaTitulacion");
-		assertThat(requisitos_edad).isEqualTo("ValorEsperadoParaRequisitosEdad");
-		assertThat(pais).isEqualTo("ValorEsperadoParaPais");
-		assertThat(photo).isEqualTo("ValorEsperadoParaPhoto");
-		assertThat(pdf).isEqualTo("ValorEsperadoParaPdf");
+	    assertThat(id).isNotNull();
+	    assertThat(cuerpo).isEqualTo("Navy");
+	    assertThat(titulacion).isEqualTo("Titulacion Universitaria");
+	    assertThat(requisitos_edad).isEqualTo("37 años maximo");
+	    assertThat(pais).isEqualTo("USA");
+	    assertThat(photo).isEqualTo("https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG");
+	    assertThat(pdf).isEqualTo("https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf");
 
 	}
+	
+	 @Test
+	 void shouldReturnAllCuerposWhenListIsRequested() {
+	     ResponseEntity<String> response = restTemplate.getForEntity("/cuerpos", String.class);
+	     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	     
+	     DocumentContext documentContext = JsonPath.parse(response.getBody());
+	     int cuerpoCount = documentContext.read("$.length()");
+	     assertThat(cuerpoCount).isEqualTo(3);
+
+	     JSONArray ids = documentContext.read("$..id");
+	     assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+
+	     JSONArray cuerpos = documentContext.read("$..cuerpo");
+	     assertThat(cuerpos).containsExactlyInAnyOrder("Navy", "Army", "Air Force");
+	     
+	     JSONArray titulacion = documentContext.read("$..titulacion");
+	     assertThat(titulacion).containsExactlyInAnyOrder("Titulacion Universitaria", "Otra Titulacion", "Licenciatura en Aeronáutica");
+	     
+	     JSONArray requisitos_edad = documentContext.read("$..requisitos_edad");
+	     assertThat(requisitos_edad).containsExactlyInAnyOrder("37 años maximo", "40 años maximo", "35 años máximo");
+	     
+	     JSONArray pais = documentContext.read("$..pais");
+	     assertThat(pais).containsExactlyInAnyOrder("USA", "UK", "USA");
+	     
+	     JSONArray photo = documentContext.read("$..photo");
+	     assertThat(photo).containsExactlyInAnyOrder("https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG", 
+	    		 "https://media.defense.gov/2021/Jan/14/2002564966/1460/1280/0/210111-N-IE405-1204.JPG", 
+	    		 "https://www.airforce.com.br/images/noticias/origem-da-forca-aerea-brasileira.jpg");
+	     
+	     JSONArray pdf = documentContext.read("$..pdf");
+	     assertThat(pdf).containsExactlyInAnyOrder("https://www.secnav.navy.mil/doni/US%20Navy%20Regulations/index.pdf", 
+	    		 "https://www.army.mil/pdf/Army_Regulations.pdf", 
+	    		 "https://www.airforce.com.br/pdf/airforce_regulations.pdf");     
+	 }
 }
