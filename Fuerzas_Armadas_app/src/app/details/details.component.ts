bdcde4462932c096b20f942b1edb67b41313e868
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-15 11:47:06 +0200

    Implemento Botón Borrar Cuerpo en Details component
    Implemento Botón Borrar Cuerpo en Details Component y cuando se activa me redirige a la página principal, actualizada.

--- Fuerzas_Armadas_app/src/app/details/details.component.ts
+++ Fuerzas_Armadas_app/src/app/details/details.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CuerposService } from '../cuerpos.service';
 import { CuerposInterface } from '../cuerposInterface';
 import { CommonModule } from '@angular/common';
+import { EventService } from '../event.service';
 
 @Component({
   selector: 'app-details',
@@ -16,13 +17,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
+  @Input() cuerpos!: CuerposInterface;
   cuerposLocation: CuerposInterface | undefined;
   applyForm: FormGroup;
   mostrarDatos = false;
 
   constructor(
     private route: ActivatedRoute,
-    private cuerposService: CuerposService
+    private router: Router,
+    private cuerposService: CuerposService,
+    private eventService: EventService
   ) {
     this.applyForm = new FormGroup({
       cuerpo: new FormControl(''),
@@ -91,5 +95,27 @@ export class DetailsComponent implements OnInit {
   cancelar(): void {
     // Cerrar el formulario de modificación
     this.mostrarDatos = false;
+  }
+
+  borrarCuerpo(id: number | string | undefined) {
+    if (id !== undefined) {
+      this.cuerposService.borrarCuerpo(id).subscribe({
+        next: () => {
+          if (this.cuerposLocation) {
+            alert(`El cuerpo "${this.cuerposLocation.cuerpo}" ha sido borrado.`);
+            this.eventService.emitCuerpoDeleted();
+            // Navega a la página principal después de borrar el cuerpo
+            this.router.navigate(['/']);
+          } else {
+            console.error('No se pudo obtener el cuerpo para mostrar el mensaje de confirmación.');
+          }
+        },
+        error: error => {
+          console.error('Error al intentar borrar el cuerpo:', error);
+        }
+      });
+    } else {
+      console.error('No se pudo borrar el cuerpo porque el ID es undefined.');
+    }
   }
 }
