127151af369466297c498aea80278dfb657257f6
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-16 15:47:33 +0200

    Cuerpos.component ObtenerPaises y actualizarPaises
    en el método ngOnInit(), se llama al método obtenerPaises(), que obtiene la lista de países utilizando el método getPaises() del servicio CuerposService y los asigna al atributo paises.
    ctualizarPais(nuevoPais: string). Este método se utiliza para actualizar el país de un cuerpo.modificarPais() del servicio CuerposService para actualizar el país del cuerpo con el nuevo país proporcionado.

--- Fuerzas_Armadas_app/src/app/cuerpos/cuerpos.component.ts
+++ Fuerzas_Armadas_app/src/app/cuerpos/cuerpos.component.ts
@@ -1,19 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CuerposInterface } from '../cuerposInterface';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CuerposService } from '../cuerpos.service';
 import { EventService } from '../event.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cuerpos',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './cuerpos.component.html',
   styleUrl: './cuerpos.component.css'
 })
-export class CuerposComponent {
+export class CuerposComponent implements OnInit {
   @Input() cuerpos!: CuerposInterface;
-  constructor(private cuerposService: CuerposService, private eventService: EventService) {}
+  paises: string[] = [];
+  constructor(private cuerposService: CuerposService, private eventService: EventService) {
+    this.obtenerPaises();
+  }
+
+  ngOnInit(): void {
+    this.obtenerPaises();
+  }
+  obtenerPaises(): void {
+    this.cuerposService.getPaises().subscribe(paises => {
+      this.paises = paises;
+    });
+  }
+
+  actualizarPais(nuevoPais: string): void {
+    const cuerpoId = typeof this.cuerpos.id === 'string' ? parseInt(this.cuerpos.id, 10) : this.cuerpos.id;
+
+    const paisAnterior = this.cuerpos.pais;
+
+    this.cuerposService.modificarPais(cuerpoId, nuevoPais).subscribe({
+      next: () => {
+        console.log(`El país del cuerpo "${this.cuerpos.cuerpo}" se actualizó a "${nuevoPais}".`);
+        this.cuerpos.pais = nuevoPais;
+      },
+      error: error => {
+        console.error('Error al intentar actualizar el país del cuerpo:', error);
+        this.cuerpos.pais = paisAnterior;
+      }
+    });
+  }
+
+
 
   borrarCuerpo(id: number | string) {
     this.cuerposService.borrarCuerpo(id).subscribe({
