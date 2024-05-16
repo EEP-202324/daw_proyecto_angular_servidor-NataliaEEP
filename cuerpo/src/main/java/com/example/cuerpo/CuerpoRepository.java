04e9dd97371e4ee44a5d56fbabede1af09f657a4
NataliaEEP <145449019+NataliaEEP@users.noreply.github.com>
2024-05-16 15:42:08 +0200

    Query para obtener Paises de la BBDD
    Query para obtener lista de paises que hay en la Base de Datos

--- cuerpo/src/main/java/com/example/cuerpo/CuerpoRepository.java
+++ cuerpo/src/main/java/com/example/cuerpo/CuerpoRepository.java
@@ -2,9 +2,13 @@ package com.example.cuerpo;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CuerpoRepository extends CrudRepository<Cuerpo, Long>, PagingAndSortingRepository<Cuerpo, Long> {
 	List<Cuerpo> findByCuerpoContainingIgnoreCase(String searchTerm);
+	
+    @Query("SELECT DISTINCT c.pais FROM Cuerpo c")
+    List<String> findAllDistinctPaises();
 }
